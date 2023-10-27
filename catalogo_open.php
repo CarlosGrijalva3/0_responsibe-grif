92fea9ee2452d4991d8b04baadd046ece9605726
CarlosGrijalva3 <Cadagral3@gmail.com>
2023-10-27 15:14:16 -0600

    Agregados archivos

---
+++ catalogo_open.php
@@ -0,0 +1,17 @@
+<!--este es el catalogo de informacion-->
+<?php
+  /* local,nombre, contraseña,usuario*/
+    /*$conexion = new mysqli("localhost","id20354909_cdga","Cdga3454!","id20354909_eluwu");*/  
+    $conexion = new mysqli("localhost","root","","universidad");
+    if($conexion){
+        echo "conexion exitosa";
+    }else{
+        echo "fallo la conecion";
+    }
+
+
+
+    /**/ 
+?>
+
+
---
+++ registro_logearte/_/cerrarsesion.php
@@ -0,0 +1,12 @@
+<!--login-->
+
+<?php
+
+    session_start();
+    session_unset();
+    session_destroy();
+
+    header("Location: index.php");
+/*inicio_php*/
+?>
+ 
